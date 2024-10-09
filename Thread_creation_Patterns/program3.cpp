6bd466aa00fdacf1f7c080a9c038066a20de2d62
NeyagapulaSiddhartha <siddarthasiddu1708@gmail.com>
2024-10-09 20:49:40 +0530

    program 3

---
+++ Thread_creation_Patterns/program3.cpp
@@ -0,0 +1,51 @@
+#include<thread>
+#include <atomic>
+#include<vector>
+#include<iostream>
+#include <pthread.h>
+
+//the else branch in the  function  "func " has a use after scope issue with variable newval.
+void func2(int *yy)
+{
+     *yy = *yy +1;
+     std::cout<<"the value of y is "<<*yy<<"  \n";
+
+}
+
+bool signal()
+{
+    return false;
+}
+
+void func(int *current)
+{
+    std::thread t1;
+    if(signal())
+    {
+     *current = *current +1;
+     std::cout<<"the value of current is "<<*current<<"  \n";
+     t1 = std::thread(func, current);
+    }
+    else{
+
+    int newval=30;
+    
+    t1 = std::thread(func2, &newval);
+     std::this_thread::sleep_for(std::chrono::milliseconds(3));
+    }
+t1.join();
+
+}
+
+int main()
+{
+    
+int  x =1;
+std::thread t1;
+
+t1 = std::thread(func, &x);
+
+t1.join();
+return 0;
+
+}
