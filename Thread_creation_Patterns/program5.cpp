cfd9fb6e65587c79cfc8ae2dfaf37bc5f8b233b1
NeyagapulaSiddhartha <siddarthasiddu1708@gmail.com>
2024-10-09 20:35:52 +0530

    program 5

---
+++ Thread_creation_Patterns/program5.cpp
@@ -0,0 +1,41 @@
+#include<thread>
+#include <atomic>
+#include<vector>
+#include<iostream>
+#include <pthread.h>
+
+//the else branch in the main function has a use after scope issue with variable y.
+
+
+bool is_signal()
+{
+    return false;
+}
+
+void func(int *y)
+{
+     *y = *y +1;
+     std::cout<<"the value of y is "<<*y<<"  \n";
+
+}
+
+int main()
+{
+    
+int  x =1;
+std::thread t1;
+
+if(is_signal()){
+    
+    t1 = std::thread(func, &x);
+}
+else{
+
+    int y =0;
+    t1 = std::thread(func, &y); 
+
+}
+t1.join();
+return 0;
+
+}
