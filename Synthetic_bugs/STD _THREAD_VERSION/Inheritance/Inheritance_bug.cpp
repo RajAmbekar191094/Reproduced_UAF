d18d2027012f099f41ee400b3395c6e8c8e14aa7
RajAmbekar191094 <raj.ambekar120@gmail.com>
2025-01-24 11:43:46 +0530

    Added virtual inheritance case

---
+++ Synthetic_bugs/STD _THREAD_VERSION/Inheritance/Inheritance_bug.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+using namespace std;
+thread t1;
+
+class Base {
+public:
+       virtual void display() {
+        cout << "Base display"<<"\n";
+    }
+};
+Base* basePtr;
+class Derived : public Base {
+public:
+    void display()  {
+        cout << "Derived display"<<"\n";
+    }
+};
+
+void threadFunction(Base *baseRef) {
+    sleep(5);
+    baseRef->display();
+}  
+void createThread()  
+{   
+    
+    Derived derivedObj; 
+    basePtr = &derivedObj;    
+    t1=thread(threadFunction,basePtr);
+     //t1.join();
+}
+int main() {
+    createThread();
+    t1.join();
+    cout << "Main thread finishes.\n";
+    return 0;
+}
